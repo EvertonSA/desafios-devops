28fa681cce775485f032dab17c18b0c2099237a5
Everton S.A <eveuca@gmail.com>
2019-09-08 20:35:29 -0300

    terraform ansible solution 0.1.0

--- terraform-ansible/destroy.sh
+++ terraform-ansible/destroy.sh
@@ -1,4 +1,5 @@
 echo "terraform destoy"
+tmp_value="allowed_source_ranges=[$IP_RANGE_ALLOW_SSH]"
 terraform destroy -auto-approve -var project_id=${PROJECT_ID} -var region=${REGION} -var zone=${ZONE} -var $tmp_value
 
 echo "deleting distributed credentials"
@@ -7,4 +8,4 @@ rm -f ansible/apiadmin.*
 
 echo "delete GCP service account"
 gcloud iam service-accounts delete \
-  apiadmin@${ROJECT-ID}.iam.gserviceaccount.com
+  apiadmin@${PROJECT-ID}.iam.gserviceaccount.com
